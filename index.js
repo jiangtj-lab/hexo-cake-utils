d07f758319e478c295949f3c5464bfe2522d71d9
jiangtj <116749895@qq.com>
2019-10-10 17:02:16 +0800

    Add loadPlugin

--- index.js
+++ index.js
@@ -2,19 +2,27 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = function (hexo,pluginDir) {
+module.exports = function (hexo, pluginDir) {
   return {
-    defaultConfigFile (key, file) {
+    defaultConfigFile(key, file) {
       let defaultConfig = yaml.safeLoad(fs.readFileSync(this.getFilePath(file)));
       let data = hexo.locals.get('data');
       hexo.config[key] = Object.assign(defaultConfig, hexo.theme.config[key], hexo.config[key], data[key]);
       return hexo.config[key];
     },
-    getFilePath (file) {
+    getFilePath(file) {
       if (pluginDir) {
         return path.join(pluginDir, file);
       }
       return file;
+    },
+    loadPlugin(name) {
+      const plugin = hexo.resolvePlugin(name)
+      hexo.loadPlugin(plugin).then(() => {
+        hexo.log.debug(`Plugin loaded: ${name}`);
+      }).catch(err => {
+        hexo.log.error({ err }, `Plugin load failed: ${name}`);
+      });
     }
   }
 }
