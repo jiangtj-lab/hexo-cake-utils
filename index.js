0307d23f2ccdc8fadff0ab41710efa7992f8c29b
Mr.J <116749895@qq.com>
2026-03-18 22:06:44 +0800

    build(deps): 升级 js-yaml 依赖至 4.1.0 并迁移至 pnpm
    - 将 js-yaml 从 3.13.1 升级到 4.1.0 以获取最新功能和修复
    - 移除 yarn.lock 文件并添加 pnpm-lock.yaml 以迁移到 pnpm 包管理器
    - 更新 index.js 中 yaml 的加载方式以兼容新版本

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
-const path = require('path');
+const yaml = require("js-yaml");
+const fs = require("fs");
+const path = require("path");
 
 const mergeLang = (ctx, lang, target) => {
   if (!target) return;
   let i18n = ctx.theme.i18n;
   let source = i18n.get(lang);
-  Object.keys(target).forEach(key => {
+  Object.keys(target).forEach((key) => {
     if (source[key]) return;
     source[key] = target[key];
     i18n.set(lang, source);
-  })
-}
+  });
+};
 
 module.exports = function (hexo, pluginDir) {
   return {
     defaultConfigFile(key, file) {
-      let defaultConfig = file ? yaml.safeLoad(fs.readFileSync(this.getFilePath(file))) : {};
-      let data = hexo.locals.get('data');
-      hexo.config[key] = Object.assign(defaultConfig, hexo.theme.config[key], hexo.config[key], data[key]);
+      let defaultConfig = file
+        ? yaml.load(fs.readFileSync(this.getFilePath(file), "utf8"))
+        : {};
+      let data = hexo.locals.get("data");
+      hexo.config[key] = Object.assign(
+        defaultConfig,
+        hexo.theme.config[key],
+        hexo.config[key],
+        data[key],
+      );
       return hexo.config[key];
     },
     getFilePath(file) {
@@ -28,22 +35,25 @@ module.exports = function (hexo, pluginDir) {
       return file;
     },
     loadPlugin(name) {
-      const plugin = hexo.resolvePlugin(name)
-      hexo.loadPlugin(plugin).then(() => {
-        hexo.log.debug(`Plugin loaded: ${name}`);
-      }).catch(err => {
-        hexo.log.error({ err }, `Plugin load failed: ${name}`);
-      });
+      const plugin = hexo.resolvePlugin(name, hexo.base_dir);
+      hexo
+        .loadPlugin(plugin, hexo.base_dir)
+        .then(() => {
+          hexo.log.debug(`Plugin loaded: ${name}`);
+        })
+        .catch((err) => {
+          hexo.log.error({ err }, `Plugin load failed: ${name}`);
+        });
     },
     i18n(langs) {
-      hexo.extend.filter.register('before_generate', function () {
+      hexo.extend.filter.register("before_generate", function () {
         let lang = hexo.config.language;
         if (Array.isArray(lang)) {
-          lang.forEach(item => mergeLang(hexo, item, langs[item]));
+          lang.forEach((item) => mergeLang(hexo, item, langs[item]));
         } else {
           mergeLang(hexo, lang, langs[lang]);
         }
       });
-    }
-  }
-}
+    },
+  };
+};
